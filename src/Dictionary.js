68178de2a47bffe5065325ee6e459bd59e02cb94
micaelajmyers <micaelajmyers@gmail.com>
2022-08-28 17:04:00 -0400

    Removed alert

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,7 +13,6 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
     //documentation:https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
